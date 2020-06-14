896013b9613f89c1adb2c1b92695d7cddb2fa7dd
Yoru Sung <spero61@gmail.com>
2020-06-15 02:02:07 +0900

    first commit ver 1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+ 글로벌인 일본IT과정 한자단어장
+
+ 2020년 06월 25일 01:58AM ver 1.0
+
+ 반응형 웹 (데스크탑 : vertical navbar, 모바일 : horizontal navbar 등)
---
+++ javascript/toggle.js
@@ -0,0 +1,27 @@
+document.querySelector('.btn1').addEventListener('click', function(e) {
+  [].map.call(document.querySelectorAll('.yomigana, .kor'), function(el) {
+    el.classList.toggle('toggled');
+  });
+});
+
+document.querySelector('.btn2').addEventListener('click', function(e) {
+  [].map.call(document.querySelectorAll('.yomigana'), function(el) {
+    el.classList.toggle('toggled');
+  });
+});
+
+document.querySelector('.btn3').addEventListener('click', function(e) {
+  [].map.call(document.querySelectorAll('.kor'), function(el) {
+    el.classList.toggle('toggled');
+  });
+});
+
+$('a[href*=#]').click(function(event) {
+  $('html, body').animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    500
+  );
+  event.preventDefault();
+});
